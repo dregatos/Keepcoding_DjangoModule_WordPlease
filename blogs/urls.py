24807637f6befa549929090a773346d3a002de8e
David Regatos <david.regatos@gmail.com>
2015-07-20 13:32:03 +0200

    login is required to access /new-post;

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from blogs.views import HomeView, NewPostView, BlogListView, BlogDetailView, PostDetailView
 
 
 urlpatterns = [
     #Blogs URLs
     url(r'^$', HomeView.as_view(), name='blogs_home'),
-    url(r'^new-post/$', NewPostView.as_view(), name='create_post'),
+    url(r'^new-post/$', login_required(NewPostView.as_view()), name='create_post'),
     url(r'^blogs/$', BlogListView.as_view(), name='blogs_list'),
     url(r'^blogs/(?P<username>[a-z0-9_-]{3,16})$', BlogDetailView.as_view(), name='blog_detail'),
     url(r'^blogs/(?P<username>[a-z0-9_-]{3,16})/(?P<pk>[0-9]+)$', PostDetailView.as_view(), name='post_detail')
